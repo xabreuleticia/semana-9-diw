4d79c5f51bd59c61da6bca33c72480abcf9b624b
Leticia Abreu <xabreu.leticia@gmail.com>
2026-05-02 20:08:26 -0300

    B.1. Base de dados (JSON)

---
+++ script.js
@@ -0,0 +1,77 @@
+const data = {
+    produtos: [
+        {
+            id:1,
+            nome:"iPhone 13",
+            preco:4500,
+            categoria:"Celulares",
+            imagem:"./imgs/iphone-13-img.jpg",
+            descricao:"Smartphone da Apple com ótimo desempenho",
+            emEstoque:true
+        },
+        {
+            id:2,
+            nome:"Samsung Galaxy S22",
+            preco:3800,
+            categoria:"Celulares",
+            imagem:"./imgs/samsung-galaxy-s22-img.png",
+            descricao:"Celular potente com excelente câmera",
+            emEstoque:true
+        },
+        {
+            id:3,
+            nome:"Notebook Dell Inspiron",
+            preco:3200,
+            categoria:"Notebooks",
+            imagem:"./imgs/notebook-dell-inspiron.png",
+            descricao:"Notebook ideal para estudos e trabalho",
+            emEstoque:true
+        },
+        {
+            id:4,
+            nome:"MacBook Air",
+            preco:7000,
+            categoria:"Notebooks",
+            imagem:"./imgs/macbook-air.png",
+            descricao:"Leve, rápido e com ótima bateria",
+            emEstoque:true
+        },
+        {
+            id:5,
+            nome:"Mouse Gamer",
+            preco:150,
+            categoria:"Acessórios",
+            imagem:"./mouse-gamer.jpg",
+            descricao:"Mouse com alta precisão para jogos",
+            emEstoque:true
+        },
+        {
+            id:6,
+            nome:"Teclado Mecânico",
+            preco:300,
+            categoria:"Acessórios",
+            imagem:"./imgs.teclado-mecanico.jpg",
+            descricao:"Teclado com switches mecânicos",
+            emEstoque:true
+        },
+        {
+            id: 7,
+            nome: "Xbox Series X",
+            preco: 4300,
+            categoria: "Games",
+            imagem: "./imgs/xbox-series-x.png",
+            descricao: "Console poderoso da Microsoft",
+            emEstoque: true
+        },
+        {
+            id:8,
+            nome:"PlayStation 5",
+            preco:4500,
+            categoria:"Games",
+            imagem:"./imgs/playstation-5.jfif",
+            descricao:"Console de última geração da Sony",
+            emEstoque:false
+        },
+        
+    ]
+};
